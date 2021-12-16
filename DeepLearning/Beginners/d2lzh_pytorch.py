bf0e2a0c4ae6f818cd7aaff4fcbebaba378e3f48
Jacob-xyb <949782197@qq.com>
2021-12-16 16:46:07 +0800

    deeplearning update

---
+++ DeepLearning/Beginners/d2lzh_pytorch.py
@@ -0,0 +1,22 @@
+import random
+import torch
+from IPython import display
+import matplotlib.pylab as plt
+
+def use_svg_display():
+    # 用矢量图显示
+    display.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
+    
+# 本函数已保存在d2lzh包中方便以后使用
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)  # 样本的读取顺序是随机的
+    for i in range(0, num_examples, batch_size):
+        j = torch.LongTensor(indices[i: min(i + batch_size, num_examples)]) # 最后一次可能不足一个batch
+        yield  features.index_select(0, j), labels.index_select(0, j)
